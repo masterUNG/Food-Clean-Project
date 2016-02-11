308f208d032484000dce943236c09962640d6ef6
masterUNG <phrombutr@gmail.com>
2016-02-11 11:42:21 +0700

    Delete All SQLite Finish

--- app/src/main/java/appewtc/masterung/cleanfoodproject/MainActivity.java
+++ app/src/main/java/appewtc/masterung/cleanfoodproject/MainActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.cleanfoodproject;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -17,6 +18,31 @@ public class MainActivity extends AppCompatActivity {
         //Create & Connected
         objMyManage = new MyManage(this);
 
+        //Test Add Value
+        //testAddValue();
+
+        //Delete All SQLite
+        deleteAllSQLite();
+
+
     }   // Main Method
+
+    private void deleteAllSQLite() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        objSqLiteDatabase.delete(MyManage.table_user, null, null);
+        objSqLiteDatabase.delete(MyManage.table_recipe, null, null);
+        objSqLiteDatabase.delete(MyManage.table_restaurant, null, null);
+    }
+
+    private void testAddValue() {
+
+        objMyManage.addUser("user", "password", "name");
+        objMyManage.addrecipe("recipe", "ingredient", "how to", "descrip", "image",
+                "name", "comment");
+        objMyManage.addRestaurant("Res", "Image", "Phone", "Address",
+                "Web", "Lat", "Lng");
+
+    }   // testAddValue
 
 }   // Main Class
--- app/src/main/java/appewtc/masterung/cleanfoodproject/MyManage.java
+++ app/src/main/java/appewtc/masterung/cleanfoodproject/MyManage.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.cleanfoodproject;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,6 +13,29 @@ public class MyManage {
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
 
+    public static final String table_user = "userTABLE";
+    public static final String table_recipe = "recipeTABLE";
+    public static final String table_restaurant = "restaurantTABLE";
+
+    public static final String column__id = "_id";
+    public static final String column_User = "User";
+    public static final String column_Password = "Password";
+    public static final String column_Name = "Name";
+    public static final String column_Recipe = "Recipe";
+    public static final String column_Ingredients = "Ingredients";
+    public static final String column_HowTo = "HowTo";
+    public static final String column_Descrip = "Descrip";
+    public static final String column_ImageRecipe = "ImageRecipe";
+    public static final String column_NameComment = "NameComment";
+    public static final String column_Comment = "Comment";
+    public static final String column_Restaurant = "Restaurant";
+    public static final String column_ImageRestaurant = "ImageRestaurant";
+    public static final String column_Phone = "Phone";
+    public static final String column_Address = "Address";
+    public static final String column_Web = "Web";
+    public static final String column_Lat = "Lat";
+    public static final String column_Lng = "Lng";
+
     public MyManage(Context context) {
 
         //Connected Database
@@ -19,7 +43,59 @@ public class MyManage {
         writeSqLiteDatabase = objMyOpenHelper.getWritableDatabase();
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
-
     }   // Constructor
+
+    public long addRestaurant(String strRestaurant,
+                              String strImage,
+                              String strPhone,
+                              String strAddress,
+                              String strWeb,
+                              String strLat,
+                              String strLng) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(column_Restaurant, strRestaurant);
+        objContentValues.put(column_ImageRestaurant, strImage);
+        objContentValues.put(column_Phone, strPhone);
+        objContentValues.put(column_Address, strAddress);
+        objContentValues.put(column_Web, strWeb);
+        objContentValues.put(column_Lat, strLat);
+        objContentValues.put(column_Lng, strLng);
+
+        return writeSqLiteDatabase.insert(table_restaurant, null, objContentValues);
+    }
+
+
+    public long addrecipe(String strRecipe,
+                          String strIngredient,
+                          String strHowTo,
+                          String strDescrip,
+                          String strImage,
+                          String strName,
+                          String strComment) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(column_Recipe, strRecipe);
+        objContentValues.put(column_Ingredients, strIngredient);
+        objContentValues.put(column_HowTo, strHowTo);
+        objContentValues.put(column_Descrip, strDescrip);
+        objContentValues.put(column_ImageRecipe, strImage);
+        objContentValues.put(column_NameComment, strName);
+        objContentValues.put(column_Comment, strComment);
+
+        return writeSqLiteDatabase.insert(table_recipe, null, objContentValues);
+    }
+
+    public long addUser(String strUser,
+                        String strPassword,
+                        String strName) {
+
+        ContentValues objContentValues = new ContentValues();
+        objContentValues.put(column_User, strUser);
+        objContentValues.put(column_Password, strPassword);
+        objContentValues.put(column_Name, strName);
+
+        return writeSqLiteDatabase.insert(table_user, null, objContentValues);
+    }
 
 }   // Main Class
